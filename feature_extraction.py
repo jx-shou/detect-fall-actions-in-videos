33e48dda7575ed8cd38cd4e940d8c9a44a8013b3
jx-shou <nishino1@hotmail.com>
2021-12-22 13:46:09 +0900

    Update feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -9,12 +9,12 @@ from network import CVAE
 
 
 batch_size = 32
-dataset = FallDataset('./dir/dataset_name')
+dataset = FallDataset('./dataset_dir/dataset_name')
 dataloader = DataLoader(dataset, shuffle=False, batch_size=batch_size)
 
 
 vae = CVAE().cuda()
-file_path = './dir/file name'
+file_path = './param_dir/file name'
 vae.load_state_dict(t.load(file_path))
 vae.eval()
 print('load param.')
@@ -41,7 +41,7 @@ mu_list = np.concatenate(mu_list, axis=0)
 lab_list = np.concatenate(lab_list, axis=0)
 print(mu_list.shape, lab_list.shape)
 
-f_path = './dir/file_name'
+f_path = './file_name'
 with open(f_path, 'wb') as f:
     pkl.dump({'mu':mu_list, 'label':lab_list}, f)
     
